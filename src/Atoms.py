9df5cd4d57885a0dbe929ca2ef3b26ae715f08f5
mcjohn974 <mcvictor@tut.by>
2022-09-10 14:40:09 +0300

    atoms

--- src/Atoms.py
+++ src/Atoms.py
@@ -1,32 +1,49 @@
-#  A* a = new A(B(2) + C("eo"))
+#  A* a = new A(B(2) + C("eo"));
 #  atoms here will be:
-#  1) B(2)
-#  2) C("eo")
-#  3) operator+(1, 2)
-#  4) new A(3)
-#  5) A* a = 4
+#  1) B;
+#  2) C;
+#  5) A* a = new A;
 #  I claim that each atom contains one or zero object creation.
+
+
 class Atoms:
+    # later, when we teach the code to make a differ between * as a multiplication and * as a pointer 
+    # here will be a '*' special character
+    __special_characters = frozenset((',', '+', '-', '+=', '-=', '<<', '>>', '/', '/=', '*='))
+
+
     def __init__(self, codeline : str) -> None:
-        pass
+        self.__atoms = self.__GenerateAtoms(codeline.replace('{', '').replace('}', ''))
+
+
+    def __GenerateAtoms(self, codeline : str) -> list:
+        for i in range(len(codeline)):
+            if codeline[i] in self.__special_characters:
+                return self.__GenerateAtoms(codeline[:i]) + self.__GenerateAtoms(codeline[i + 1:])
+            if codeline[i] == '(':
+                j = self.__FindClose(i)
+                return self.__GenerateAtoms(codeline[:i] + codeline[j + 1:]) + self.__GenerateAtoms(codeline[i+1:j])
+        return [codeline]
 
 
     class Iterator:
-        def __init__(self) -> None:
-            pass
+        def __init__(self, lst : list, len : int, pos : int) -> None:
+            self.__lst = lst
+            self.__len = len
+            self.__pos = pos
 
 
         def Next(self) -> None:
-            pass
+            self.__pos += 1
 
 
         def Code(self) -> str:
-            pass
+            return self.__lst[self.__pos]
 
 
         def IsEnd(self) -> bool:
-            pass
+            return self.__pos >= self.__len
 
 
     def Begin(self) -> Iterator:
-        pass
+        return self.Iterator(self.__atoms, len(self.__atoms), 0)
